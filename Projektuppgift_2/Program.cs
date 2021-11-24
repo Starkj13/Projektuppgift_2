90de53d5af390e15f8a6e194840364516d62fa44
Starkj13 <kjellqvist13@gmail.com>
2021-11-24 14:01:36 +0100

    Test

--- Projektuppgift_2/Program.cs
+++ Projektuppgift_2/Program.cs
@@ -6,16 +6,8 @@ namespace Projektuppgift_2
     {
         static void Main(string[] args)
         {
-            /* En meny med menyval 1.visa bord 2.lägg till/ändra bordsinformation 3.Markera ett bord som tomt 4.Avsluta */
-            int Kör = 1;
-            
-            
-            while (Kör == 1 )
-            {
-                Console.WriteLine("Välj ett av menyvalena \n 1.visa bordena \n 2.lägg till/ändra bordsinformation \n 3.Markera ett bord som tomt \n 4.Avsluta");
-
-
-            } 
+            Console.WriteLine("hello world");
+            hasn asdbn as DataMisalignedException 
         }
     }
 }
